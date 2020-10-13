f8128b03e04c0b39d38c664e85e7105e8113077d
Naufal Prima Yoriko <13518146@std.stei.itb.ac.id>
2020-10-13 16:36:53 +0700

    add db_migrate

---
+++ README.md
@@ -0,0 +1,31 @@
+# Willy Wangky Chocholate Online Shop
+
+-------
+Intro
+
+## Dependency
+
+-------
+
+1. sad
+2. sad
+3. sad
+
+
+## How to Run
+
+-------
+
+1. sad
+2. sad
+3. sad
+
+
+## How to Use
+
+-------
+
+1. sad
+2. sad
+3. sad
+4. 
---
+++ init_db.php
@@ -0,0 +1,65 @@
+<?php
+
+// Connection query change as your own needs
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "wbd1";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("conn failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE TABLE users ( 
+    username VARCHAR(50) PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    passwordhash VARCHAR(100) NOT NULL,
+    superuser BOOLEAN NOT NULL
+    )";
+
+if ($conn->query($sql) === TRUE){
+    echo "users table successfully created";
+} else {
+    echo "ERROR create table users"
+}
+
+$sql = "CREATE TABLE chocolates ( 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    amount INT(10) UNSIGNED NOT NULL,
+    price INT(12) UNSIGNED NOT NULL,
+    description TEXT,
+    image LONGBLOB
+    )";
+
+if ($conn->query($sql) === TRUE){
+    echo "chocolates table successfully created";
+} else {
+    echo "ERROR create table chocolates"
+}
+
+$sql = "CREATE TABLE transactions ( 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    choco_id INT(6) UNSIGNED,
+    amount INT(10) UNSIGNED NOT NULL,
+    totalprice INT(18) UNSIGNED NOT NULL,
+    description TEXT,
+    address TEXT NOT NULL,
+    time DATETIME NOT NULL,
+    FOREIGN KEY (choco_id) REFERENCES chocolates(id)
+    )";
+
+// salat satu cara buat timestamp : 
+// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+if ($conn->query($sql) === TRUE){
+    echo "transactions table successfully created";
+} else {
+    echo "ERROR create table transactions"
+}
+
+$conn->close();
+
+?>
