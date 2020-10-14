b3f10983db0a97d66e110103819540b07d472433
primayoriko <primayoriko@gmail.com>
2020-10-14 08:36:46 +0700

    structure project + create register ui

--- config/init_db.php
+++ config/init_db.php
@@ -15,11 +15,11 @@ if($conn->connect_error){
 }
 
 $sql = "CREATE TABLE users ( 
-    username VARCHAR(50) PRIMARY KEY,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    passwordhash VARCHAR(100) NOT NULL,
-    superuser BOOLEAN NOT NULL
-    )";
+            username VARCHAR(50) PRIMARY KEY,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            passwordhash VARCHAR(100) NOT NULL,
+            superuser BOOLEAN NOT NULL
+        )";
 
 if ($conn->query($sql) === TRUE){
     echo "users table successfully created";
@@ -28,13 +28,13 @@ if ($conn->query($sql) === TRUE){
 }
 
 $sql = "CREATE TABLE chocolates ( 
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    amount INT(10) UNSIGNED NOT NULL,
-    price INT(12) UNSIGNED NOT NULL,
-    description TEXT,
-    image LONGBLOB
-    )";
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            amount INT(10) UNSIGNED NOT NULL,
+            price INT(12) UNSIGNED NOT NULL,
+            description TEXT,
+            image LONGBLOB
+        )";
 
 if ($conn->query($sql) === TRUE){
     echo "chocolates table successfully created\n";
@@ -43,15 +43,17 @@ if ($conn->query($sql) === TRUE){
 }
 
 $sql = "CREATE TABLE transactions ( 
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    choco_id INT(6) UNSIGNED,
-    amount INT(10) UNSIGNED NOT NULL,
-    totalprice INT(18) UNSIGNED NOT NULL,
-    description TEXT,
-    address TEXT NOT NULL,
-    time DATETIME NOT NULL,
-    FOREIGN KEY (choco_id) REFERENCES chocolates(id)
-    )";
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50) NOT NULL,
+            choco_id INT(6) UNSIGNED NOT NULL,
+            amount INT(10) UNSIGNED NOT NULL,
+            totalprice INT(18) UNSIGNED NOT NULL,
+            description TEXT,
+            address TEXT NOT NULL,
+            time DATETIME NOT NULL,
+            FOREIGN KEY (choco_id) REFERENCES chocolates(id),
+            FOREIGN KEY (username) REFERENCES users(username)
+        )";
 
 // salat satu cara buat timestamp : 
 // reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- register.php
+++ register.php
@@ -1,3 +1,61 @@
 <?php
+    require_once "db_config.php";
 
+    $username = "";
+    $password = "";
+    $email = "";
+
+    $username_err = "";
+    $email_err = "";
+    $password_err = "";
+    $confirm_pass_err = "";
+
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        if(empty(trim($username))){
+            $username_err = "username must be filled!";
+        } else {
+            $sql = "SELECT id FROM users WHERE username = :username";
+            $stm = $pdo->prepare($sql);
+
+            $username_data = trim($_POST["username"]);
+            $stm->bindParam(":username", $username_data);
+
+            if($stm->execute()){
+                if($stm->rowCount() === 1){
+                    $username_err = "username already taken.";
+                } else {
+                    $username = $username_data;
+                }
+            } else {
+                echo "Internal server error";
+            }
+
+            unset($stm);
+        }
+
+        if(empty(trim($_POST["password"]))){
+            $password_err = "password should be filled";
+        } else {
+            $password = trim($_POST["password"]);
+        }
+
+        if(empty(trim($_POST["confirm_password"]))){
+            $confirm_pass_err = "please input confirmation of the password";
+        }
+        else if($_POST["password"] !== $_POST["corfirm_password"]
+            && empty($password_err)){
+            $confirm_pass_err = "the inputted string not same";
+        }
+
+        // ADD EMAIL VALIDATION
+
+        if(!empty($confirm_pass_err) || !empty($password_err) || 
+            !empty($username_err) || !empty($email_err)){
+
+            }
+    } else {
+
+    }
+
+    readfile('public/page/register.html');
 ?>
