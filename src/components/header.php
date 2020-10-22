2b17828bc6ca22345f463cb0fd753b6f53b8500a
BaconLover307 <kresnadi30@gmail.com>
2020-10-22 19:00:38 +0700

    Created base for header bar, initialised dashboard

---
+++ src/components/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../../public/css/header.css" type="text/css">
+</head>
+
+<body>
+    <header class="text-title">
+        <div class="header-comp">
+            <div class="header-menu" id="homeButton">
+                Home
+            </div>
+            <?php
+            if (isset($_SESSION['isSuperuser'])) {
+                if ($_SESSION["isSuperuser"]) {
+                    echo '<div class="header-menu"> Add New Chocolate </div>';
+                } else {
+                    echo '<div class="header-menu"> History </div>';
+                }
+            }
+            ?>
+        </div>
+        <div class="header-comp">
+            Search Bar
+        </div>
+        <div class="header-comp header-menu" id="logoutButton">
+            Logout
+        </div>
+    </header>
+    <script>
+
+    </script>
+
+</body>
+
+</html>
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,32 +1,41 @@
 <?php
-    session_start();
+session_start();
+if (!isset($_SESSION["loggedIn"]) || !$_SESSION["loggedIn"]) {
+    header("location: login.php");
+    exit;
+}
 
-    if(!isset($_SESSION["loggedIn"]) || !$_SESSION["loggedIn"]){
-        header("location: login.php");
-        exit;
-    }
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Willy Wangky Chocolate Factory</title>
-        <link rel="stylesheet" href="../public/css/index.css" type="text/css">
-        <link rel="stylesheet" href="../public/css/dashboard.css" type="text/css">
-    </head>
-    <body>
 
-        <div class="spacer"></div>
-        <header>
-            Willy Wangky Choco Factory
-        </header>
-        <div class="spacer"></div>
-        <main>
+<head>
+    <title>Willy Wangky Chocolate Factory</title>
+    <link rel="stylesheet" href="../public/css/index.css" type="text/css">
+    <link rel="stylesheet" href="../public/css/dashboard.css" type="text/css">
+    <!-- <link rel="stylesheet" href="../public/css/login.css" type="text/css"> -->
+    <!-- <link rel="stylesheet" href="../public/css/register.css" type="text/css"> -->
+</head>
 
-        </main>
-        <div class="spacer"></div>
+<body>
+    <main>
+        <?php include('components/header.php') ?>
+        <div class="container">
+            <div class="top-container text-title">
+                <?php
+                echo ('<h2 class="top-content">Hello, ' . $_SESSION["username"] . '</h2>')
+                ?>
+                <div class="top-content" id="buttonViewAll">View all chocolates</div>
+            </div>
+        </div>
+
         <script>
-            
-        </script>    
-    </body>
+
+        </script>
+    </main>
+</body>
+
 </html>
