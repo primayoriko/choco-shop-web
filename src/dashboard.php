059a73f37aa4c3f46d23292c7e1483c4ec2c5df1
BaconLover307 <kresnadi30@gmail.com>
2020-10-23 15:37:18 +0700

    Update base on Add and Buy Chocolate, added buttons.css

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if (!isset($_SESSION["loggedIn"]) || !$_SESSION["loggedIn"]) {
-    header("location: login.php");
-    exit;
-}
+// session_start();
+// if (!isset($_SESSION["loggedIn"]) || !$_SESSION["loggedIn"]) {
+//     header("location: login.php");
+//     exit;
+// }
 
 
 ?>
